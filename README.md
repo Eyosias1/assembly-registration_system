9c4a4242bd1a831a43aa3550780aa66c7463fb1b
Eyosias Woldemichael <62175185+Eyosias1@users.noreply.github.com>
2024-03-04 07:55:14 +0000

    finish writting readme file added writting functions

--- README.md
+++ README.md
@@ -1,6 +1,59 @@
-# assembly-registration_system
-## Pour compiler
-```shell
-sudo apt install nasm
-sudo update
-```
+# Système d'Enregistrement des Employés
+Ce programme en langage d'assemblage permet d'enregistrer des employés, de lister tous les employés enregistrés, d'afficher un employé spécifique, de trouver l'age moyen des employer et de quitter le programme.
+
+1. Installer nasm si on a pas fait:
+    ```
+    sudo apt install nasm
+    ```
+2. Assemblez le programme en utilisant NASM :
+    ```
+    nasm -f elf sys-inlist.asm
+    ```
+3. Liez le fichier objet pour créer l'exécutable :
+    ```
+     ld -m elf_i386 -o sys-inlist.elf sys-inlist.o
+    ```
+4. Exécutez le programme :
+    ```
+    ./sys-inlist.elf 
+    ```
+
+## Fonctionnalités
+
+- **Enregistrer un employé**: Permet d'ajouter un nouvel employé avec son nom et âge.
+- **Lister tous les employés**: Affiche la liste de tous les employés enregistrés.
+- **Afficher un employé spécifique**: Recherche un employé par son identifiant.
+- **Trouver l'age moyen**: Identifie l'age moyen de tout les employer.
+- **Quitter**: Termine l'exécution du programme.
+
+## Utilisation
+
+Pour exécuter ce programme, vous devez avoir un assembleur x86 et un environnement capable d'exécuter des programmes en langage d'assemblage. Voici les étapes à suivre pour assembler et exécuter le programme :
+
+1. Assemblez le programme avec votre assembleur préféré. Par exemple, en utilisant NASM :
+    ```
+    nasm -f elf64 mon_programme.asm -o mon_programme.o
+    ```
+2. Liez le fichier objet pour créer l'exécutable :
+    ```
+    ld mon_programme.o -o mon_programme
+    ```
+3. Exécutez le programme :
+    ```
+    ./mon_programme
+    ```
+
+## Structure du Projet
+
+- **.data section**: Contient toutes les données statiques utilisées par le programme, y compris les menus et messages affichés à l'utilisateur.
+- **.text section**: Contient le code exécutable du programme, y compris les procédures pour chaque fonctionnalité.
+
+
+## Enregistrement d'un employer
+
+
+## Auteurs
+
+- **Woldemichael Eyosias**
+- **Djiakité Madina**
+
--- sys-inlist.asm
+++ sys-inlist.asm
@@ -5,7 +5,24 @@ menu: db "1. Enregistrer un employé", 10
       db "4. Afficher âge moyen", 10
       db "5. Quitter", 10
 menuLength: equ $-menu
-
+message_enregistre: db "Enregistrement du personnel :", 10
+message_enregistreLength: equ $-message_enregistre
+message_nom_Age: db "Nom et Age :", 32
+message_nom_AgeLength: equ $-message_nom_Age
+message_Id: db "Identifiant :", 32
+message_IdLength: equ $-message_Id
+message_liste: db "Liste des personnes :", 10
+message_listeLength: equ $-message_liste
+message_cherche: db "Cherche la personne :", 10
+message_chercheLength: equ $-message_cherche
+message_alert: db "No such person!", 10
+message_alertLength: equ $-message_alert
+message_age: db "Age en moyenne :", 10
+message_ageLength: equ $-message_age
+message_menu_choix: db "Votre choix :", 32
+message_menu_choixLength: equ $-message_menu_choix
+message_alert_choix: db 9,9,9,"MAUVAISE SAISIE ECRIVER UN CHIFFRE !!! ", 10
+message_alert_choixLength: equ $-message_alert_choix
 section .text
 
 global _start
@@ -25,10 +42,46 @@ _start:
     ;je quitterProgramme
     jmp _start
 
+;/////////////////////////////////////////////
+;         fonction de affichage de menu      /
+;                                            /
+;                                            /
+;/////////////////////////////////////////////
 afficherMenu:
     mov eax, 4                                      ; syscall write
     mov ebx, 1                                      ; fd (file descriptor) pour stdout
     mov ecx, menu                                   ; Pointeur vers le début du message à afficher
     mov edx, menuLength                             ; Longueur du message à afficher
+    int 0x80                                        ; Appeler le kernel Linux
+    ret
+
+afficheMessageVotre_choix:
+    mov eax, 4                                      ; syscall write
+    mov ebx, 1                                      ; fd (file descriptor) pour stdout
+    mov ecx, message_menu_choix                                   ; Pointeur vers le début du message à afficher
+    mov edx, message_menu_choixLength                             ; Longueur du message à afficher
+    int 0x80                                        ; Appeler le kernel Linux
+    ret
+afficheMessageVotre_choix_alert:
+    mov eax, 4                                      ; syscall write
+    mov ebx, 1                                      ; fd (file descriptor) pour stdout
+    mov ecx, message_alert_choix                                   ; Pointeur vers le début du message à afficher
+    mov edx, message_alert_choixLength                             ; Longueur du message à afficher
+    int 0x80                                        ; Appeler le kernel Linux
+    ret
+
+afficheMessageNomAge:
+    mov eax, 4                                      ; syscall write
+    mov ebx, 1                                      ; fd (file descriptor) pour stdout
+    mov ecx, message_nom_Age                                   ; Pointeur vers le début du message à afficher
+    mov edx, message_nom_AgeLength                             ; Longueur du message à afficher
+    int 0x80                                        ; Appeler le kernel Linux
+    ret
+
+afficheMessageID:
+    mov eax, 4                                      ; syscall write
+    mov ebx, 1                                      ; fd (file descriptor) pour stdout
+    mov ecx, message_Id                                   ; Pointeur vers le début du message à afficher
+    mov edx, message_IdLength                             ; Longueur du message à afficher
     int 0x80                                        ; Appeler le kernel Linux
     ret
